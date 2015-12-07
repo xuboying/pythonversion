4cf37853a7e11e58b367322277e24139fd3d8634
Boying Xu <xuboying@gmail.com>
2015-12-07 16:02:02 +0800

    update

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pyversion',
+  packages = ['pyversion'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Check Python version',
+  author = 'Boying Xu',
+  author_email = 'xuboying@gmail.com',
+  url = 'https://github.com/xuboying/pyversion/', # use the URL to the github repo
+  download_url = 'https://github.com/xuboying/pyversion/tarball/0.1', # I'll explain this in a second
+  keywords = ['version'], # arbitrary keywords
+  classifiers = [],
+)
--- test.py
+++
@@ -1,4 +0,0 @@
-#!python
-import pyversion
-pyversion.PyVersionCheck(min="2.7.5",max="2.7.6")
-print "foo"
