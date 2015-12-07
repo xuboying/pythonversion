d6828f473d65066915a50cd80e40a94db9415de6
Boying Xu <xuboying@gmail.com>
2015-12-07 15:46:33 +0800

    innit

---
+++ pyversion.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+import sys
+
+def _PrintErrorAndExit(min,max):
+    if max == "":
+        sys.exit("This script requires Python version %s or newer" % min)
+    else:
+        sys.exit("The supported Python version for this script is from %s to %s" % (min,max))
+
+def PyVersionCheck(min,max=""):
+    _min = min.split(".")
+    for i in range(0,len(_min)):   
+        if sys.version_info[i] < int(_min[i]):
+            _PrintErrorAndExit(min,max)
+    if max == "":
+        return
+    _max = max.split(".")
+    for i in range(0,len(_max)):
+        if sys.version_info[i] < int(_max[i]):
+            return
+        elif sys.version_info[i] > int(_max[i]):
+            _PrintErrorAndExit(min,max)
---
+++ test.py
@@ -0,0 +1,4 @@
+#!python
+import pyversion
+pyversion.PyVersionCheck(min="2.7.5",max="2.7.6")
+print "foo"
